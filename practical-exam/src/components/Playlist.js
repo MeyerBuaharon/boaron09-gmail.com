a870ab0b4a349ee921a879a1dbbbbc9d67389c04
unknown <boaron09@gmail.com>
2020-03-29 22:49:52 +0300

    add mock photo to illustrate the reasoning of right side of the player

--- practical-exam/src/components/Playlist.js
+++ practical-exam/src/components/Playlist.js
@@ -1,6 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 
-const Playlist = () => <div>playlist</div>;
+const Image = styled.img`
+  width: 500px;
+`;
+
+const Playlist = () => <Image src={require("../assets/download.jpg")} />;
 
 export default Playlist;
